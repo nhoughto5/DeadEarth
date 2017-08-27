f6ee4e10882877024b25395521ec0ebcbdd7f7f4
Nick <nhoughto5@gmail.com>
2017-08-27 16:24:49 -0700

    Finished lecture 16

---
+++ Assets/Dead Earth/Scripts/AI/AIState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AIState : MonoBehaviour
+{
+    protected AIStateMachine _stateMachine;
+
+    public void SetStateMachine(AIStateMachine machine) { _stateMachine = machine; }
+    public abstract AIStateType GetStateType();
+
+    // Default Handlers
+    public virtual void OnEnterState() { }
+    public virtual void OnExitState() { }
+    public virtual void OnAnimatorUpdated() { }
+    public virtual void OnAnimatorIKUpdated() { }
+    public virtual void OnTriggerEvent(AITriggerEventType eventType, Collider other) { }
+    public virtual void OnDestinationReached(bool isReached) { }
+
+    public abstract AIStateType OnUpdate();
+}
---
+++ Assets/Dead Earth/Scripts/AI/AIStateMachine.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum AIStateType { None, Idle, Alerted, Patrol, Attack, Feeding, Pursuit, Dead };
+public enum AITargetType { None, Waypoint, VisualPlayer, VisualLight, VisualFood, Audio };
+public enum AITriggerEventType { Enter, Stay, Exit };
+
+public struct AITarget
+{
+    private AITargetType _type;
+    private Collider _collider;
+    private Vector3 _position;
+    private float _distance;
+    private float _time;
+
+
+    public AITargetType type { get { return _type; } }
+    public Collider collider { get { return _collider; } }
+    public Vector3 position { get { return _position; } }
+    public float distance { get { return _distance; } set { _distance = value; } }
+    public float time { get { return _time; } }
+
+    public void Set(AITargetType t, Collider c, Vector3 p, float d)
+    {
+        _type = t;
+        _collider = c;
+        _position = p;
+        _distance = d;
+        _time = Time.time;
+    }
+
+    public void Clear()
+    {
+        _type = AITargetType.None;
+        _collider = null;
+        _position = Vector3.zero;
+        _distance = Mathf.Infinity;
+        _time = Time.time;
+    }
+}
+
+public abstract class AIStateMachine : MonoBehaviour
+{
+    // Public
+    public AITarget VisualThreat = new AITarget();
+    public AITarget AudioThreat = new AITarget();
+
+
+    // Protected
+    protected Dictionary<AIStateType, AIState> _states = new Dictionary<AIStateType, AIState>();
+    protected AITarget _target = new AITarget();
+    protected AIState _currentState = null;
+
+    [SerializeField] protected AIStateType _currentStateType = AIStateType.Idle;
+    [SerializeField] protected SphereCollider _targetTrigger = null;
+    [SerializeField] protected SphereCollider _sensorTrigger = null;
+    [SerializeField] [Range(0, 15)] protected float _stoppingDistance = 1.0f;
+
+    // Component Cache
+    protected Animator _animator = null;
+    protected NavMeshAgent _navAgent = null;
+    protected Collider _collider = null;
+    protected Transform _transform = null;
+
+    public Animator GetAnimator { get { return _animator; } }
+    public NavMeshAgent GetNavAgent { get { return _navAgent; } }
+
+    // Use this for initialization
+    protected virtual void Start()
+    {
+        AIState[] states = GetComponents<AIState>();
+        foreach (AIState state in states)
+        {
+            if (state != null && !_states.ContainsKey(state.GetStateType()))
+            {
+                _states[state.GetStateType()] = state;
+                state.SetStateMachine(this);
+            }
+        }
+
+        if (_states.ContainsKey(_currentStateType))
+        {
+            _currentState = _states[_currentStateType];
+            _currentState.OnEnterState();
+        }
+        else
+        {
+            _currentState = null;
+        }
+    }
+
+    // Update is called with every tick of the physics system
+    protected virtual void FixedUpdate()
+    {
+        VisualThreat.Clear();
+        AudioThreat.Clear();
+
+        if (_target.type != AITargetType.None)
+        {
+            _target.distance = Vector3.Distance(_transform.position, _target.position);
+        }
+    }
+
+    protected virtual void Update()
+    {
+        if (_currentState == null) return;
+
+        AIStateType newStateType = _currentState.OnUpdate();
+        if (newStateType != _currentStateType)
+        {
+            AIState newState = null;
+            if (_states.TryGetValue(newStateType, out newState))
+            {
+                _currentState.OnExitState();
+                newState.OnEnterState();
+                _currentState = newState;
+            }
+            else if (_states.TryGetValue(AIStateType.Idle, out newState))
+            {
+                _currentState.OnExitState();
+                newState.OnEnterState();
+                _currentState = newState;
+            }
+            _currentStateType = newStateType;
+        }
+    }
+
+    public void SetTarget(AITargetType t, Collider c, Vector3 p, float d)
+    {
+        _target.Set(t, c, p, d);
+        if (_targetTrigger != null)
+        {
+            _targetTrigger.radius = _stoppingDistance;
+            _targetTrigger.transform.position = _target.position;
+            _targetTrigger.enabled = true;
+        }
+    }
+
+    public void SetTarget(AITargetType t, Collider c, Vector3 p, float d, float s)
+    {
+        _target.Set(t, c, p, d);
+        if (_targetTrigger != null)
+        {
+            _targetTrigger.radius = s;
+            _targetTrigger.transform.position = _target.position;
+            _targetTrigger.enabled = true;
+        }
+    }
+
+    public void SetTarget(AITarget t)
+    {
+        _target = t;
+        if (_targetTrigger != null)
+        {
+            _targetTrigger.radius = _stoppingDistance;
+            _targetTrigger.transform.position = t.position;
+            _targetTrigger.enabled = true;
+        }
+    }
+
+    public void ClearTarget()
+    {
+        _target.Clear();
+        if (_targetTrigger != null)
+        {
+            _targetTrigger.enabled = false;
+        }
+    }
+}
