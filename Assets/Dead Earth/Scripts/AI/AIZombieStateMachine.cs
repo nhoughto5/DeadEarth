4aca8d56dd5e79981cc54bec43d079e233ec9640
Nick <nhoughto5@gmail.com>
2017-10-01 18:52:21 -0700

    Feeding 1 done

--- Assets/Dead Earth/Scripts/AI/AIZombieStateMachine.cs
+++ Assets/Dead Earth/Scripts/AI/AIZombieStateMachine.cs
@@ -1,63 +1,59 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 // --------------------------------------------------------------------------
 // CLASS	:	AIZombieStateMachine
 // DESC		:	State Machine used by zombie characters
 // --------------------------------------------------------------------------
-public class AIZombieStateMachine : AIStateMachine
-{
-    // Inspector Assigned
-    [SerializeField] [Range(10.0f, 360.0f)] float _fov = 50.0f;
-    [SerializeField] [Range(0.0f, 1.0f)] float _sight = 0.5f;
-    [SerializeField] [Range(0.0f, 1.0f)] float _hearing = 1.0f;
-    [SerializeField] [Range(0.0f, 1.0f)] float _aggression = 0.5f;
-    [SerializeField] [Range(0, 100)] int _health = 100;
-    [SerializeField] [Range(0.0f, 1.0f)] float _intelligence = 0.5f;
-    [SerializeField] [Range(0.0f, 1.0f)] float _satisfaction = 1.0f;
+public class AIZombieStateMachine : AIStateMachine {
 
     // Private
-    private int _seeking = 0;
     private bool _feeding = false;
     private bool _crawling = false;
+    [SerializeField] [Range(0.0f, 1.0f)] private float _aggression = 0.5f;
+    [SerializeField] private float _depletionRate = 0.1f;
+    private int _seeking = 0;
     private int _attackType = 0;
 
+    // Inspector Assigned
+    [SerializeField] [Range(10.0f, 360.0f)] private float _fov = 50.0f;
+    [SerializeField] [Range(0, 100)] private int _health = 100;
+    [SerializeField] [Range(0.0f, 1.0f)] private float _hearing = 1.0f;
+    [SerializeField] [Range(0.0f, 1.0f)] private float _intelligence = 0.5f;
+    [SerializeField] private float _replenishRate = 0.5f;
+    [SerializeField] [Range(0.0f, 1.0f)] private float _satisfaction = 1.0f;
+
     // Hashes
-    private int _speedHash = Animator.StringToHash("Speed");
     private int _seekingHash = Animator.StringToHash("Seeking");
+    private int _speedHash = Animator.StringToHash("Speed");
     private int _feedingHash = Animator.StringToHash("Feeding");
     private int _attackHash = Animator.StringToHash("Attack");
+    [SerializeField] [Range(0.0f, 1.0f)] private float _sight = 0.5f;
     private float _speed = 0.0f;
 
     // Public Properties
-    public float fov { get { return _fov; } }
-    public float hearing { get { return _hearing; } }
-    public float sight { get { return _sight; } }
-    public bool crawling { get { return _crawling; } }
-    public float intelligence { get { return _intelligence; } }
-    public float satisfaction { get { return _satisfaction; } set { _satisfaction = value; } }
     public float aggression { get { return _aggression; } set { _aggression = value; } }
-    public int health { get { return _health; } set { _health = value; } }
     public int attackType { get { return _attackType; } set { _attackType = value; } }
+    public bool crawling { get { return _crawling; } }
     public bool feeding { get { return _feeding; } set { _feeding = value; } }
+    public float fov { get { return _fov; } }
+    public int health { get { return _health; } set { _health = value; } }
+    public float hearing { get { return _hearing; } }
+    public float intelligence { get { return _intelligence; } }
+    public float replenishRate { get { return _replenishRate; } }
+    public float satisfaction { get { return _satisfaction; } set { _satisfaction = value; } }
     public int seeking { get { return _seeking; } set { _seeking = value; } }
-    public float speed
-    {
-        get { return _speed; }
-        set { _speed = value; }
-    }
+    public float sight { get { return _sight; } }
+    public float speed {get { return _speed; }set { _speed = value; }}
 
     // ---------------------------------------------------------
     // Name	:	Update
     // Desc	:	Refresh the animator with up-to-date values for
     //			its parameters
     // ---------------------------------------------------------
-    protected override void Update()
-    {
+    protected override void Update() {
         base.Update();
 
-        if (_animator != null)
-        {
+        if (_animator != null) {
             _animator.SetFloat(_speedHash, _speed);
             _animator.SetBool(_feedingHash, _feeding);
             _animator.SetInteger(_seekingHash, _seeking);
---
+++ Assets/Dead Earth/Scripts/AI/AIZombieState_Feeding1.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class AIZombieState_Feeding1 : AIZombieState {
+    private int _eatingLayerIndex = -1;
+    private int _eatStateHash = Animator.StringToHash("Feeding State");
+    [SerializeField] private float _slerpSpeed = 5.0f;
+
+    public override AIStateType GetStateType() {
+        return AIStateType.Feeding;
+    }
+
+    public override void OnEnterState() {
+        Debug.Log("Entering Feeding State");
+        base.OnEnterState();
+        if (_zombieStateMachine == null)
+            return;
+        if (_eatingLayerIndex == -1) {
+            _eatingLayerIndex = _zombieStateMachine.animator.GetLayerIndex("Cinematic");
+        }
+        _zombieStateMachine.feeding = true;
+        _zombieStateMachine.seeking = 0;
+        _zombieStateMachine.speed = 0;
+        _zombieStateMachine.attackType = 0;
+
+        _zombieStateMachine.NavAgentControl(true, false);
+    }
+
+    public override void OnExitState() {
+        if (_zombieStateMachine != null)
+            _zombieStateMachine.feeding = false;
+    }
+
+    public override AIStateType OnUpdate() {
+        if (_zombieStateMachine.satisfaction > 0.9f) {
+            // Sets waypoint as next target
+            _zombieStateMachine.GetWaypointPosition(false);
+            return AIStateType.Alerted;
+        }
+
+        if (_zombieStateMachine.VisualThreat.type != AITargetType.None && _zombieStateMachine.VisualThreat.type != AITargetType.Visual_Food) {
+            _zombieStateMachine.SetTarget(_zombieStateMachine.VisualThreat);
+            return AIStateType.Alerted;
+        }
+
+        if (_zombieStateMachine.AudioThreat.type == AITargetType.Audio) {
+            _zombieStateMachine.SetTarget(_zombieStateMachine.AudioThreat);
+            return AIStateType.Alerted;
+        }
+
+        if (_zombieStateMachine.animator.GetCurrentAnimatorStateInfo(_eatingLayerIndex).shortNameHash == _eatStateHash) {
+            _zombieStateMachine.satisfaction = Mathf.Min(_zombieStateMachine.satisfaction + 0.01f * (Time.deltaTime * _zombieStateMachine.replenishRate), 1.0f);
+        }
+
+        if (!_zombieStateMachine.useRootRotation) {
+            Vector3 targetPos = _zombieStateMachine.targetPosition;
+            targetPos.y = _zombieStateMachine.transform.position.y;
+            Quaternion newRot = Quaternion.LookRotation(targetPos - _zombieStateMachine.transform.position);
+            _zombieStateMachine.transform.rotation = Quaternion.Slerp(_zombieStateMachine.transform.rotation, newRot, Time.deltaTime * _slerpSpeed);
+        }
+        return AIStateType.Feeding;
+    }
+}
